b4f5d048a38c63bfdd2b611fa48967e6d6f825a4
Star_kücki <reutertine@gmail.com>
2026-07-01 12:30:00 +0200

    rename Api Adress

--- app/src/main/java/com/example/bankingapp/data/remote/ApiClient.java
+++ app/src/main/java/com/example/bankingapp/data/remote/ApiClient.java
@@ -6,7 +6,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://10.0.2.2:8080/";
+    //private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL = "http://100.89.37.106:8080/";
+
     private static BankApiService service;
     private static String cachedUser;
     private static String cachedPassHash;
